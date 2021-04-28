5be2d6752e9dc55a4f5a8148ca5494f8945a49d0
José Victor T. Ferreira <josevictortf@gmail.com>
2021-04-28 13:13:44 -0300

    build: changing server

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import { AddressInfo } from "net";
 import { app } from "./app";
 
-const server = app.listen(3333, () => {
+const { PORT = 3333 } = process.env;
+
+const server = app.listen(PORT, () => {
   if (server) {
     const address = server.address() as AddressInfo;
-    console.log(`Server running in http://localhost:${address.port}`);
+    console.log(`Server running in http://localhost:${PORT}`);
   } else {
     console.error(`Failure upon starting server.`);
   }
