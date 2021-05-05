24c68fe813b9c09df27103b49cd445afe986cb92
José Victor T. Ferreira <josevictortf@gmail.com>
2021-05-05 14:19:25 -0300

    feat: implement playlist router

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Express } from "express";
 import cors from "cors";
 import { userRouter } from "./routes/userRouter";
 import { musicRouter } from "./routes/musicRouter";
+import { playlistRouter } from "./routes/playlistRouter";
 
 const app: Express = express();
 
@@ -10,5 +11,6 @@ app.use(cors());
 
 app.use("/user", userRouter);
 app.use("/music", musicRouter);
+app.use("/playlist", playlistRouter);
 
 export { app };
---
+++ src/routes/playlistRouter.ts
@@ -0,0 +1,8 @@
+import express from "express";
+import { PlaylistController } from "../controller/PlaylistController";
+
+export const playlistRouter = express.Router();
+
+const playlistController = new PlaylistController();
+
+playlistRouter.post("/create", playlistController.createPlaylist);
